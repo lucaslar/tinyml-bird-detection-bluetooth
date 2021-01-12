d4ffd25495610bd921158d2ba1bac2293b2c821c
Lucas Larisch <lucas.larisch@googlemail.com>
2021-01-12 02:55:28 +0100

    Improved language selection dialog.

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -4,7 +4,6 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
 
 /**
  * Module to be used for exporting Material components.
@@ -16,7 +15,6 @@ import { MatListModule } from '@angular/material/list';
         MatButtonModule,
         MatDialogModule,
         MatIconModule,
-        MatListModule,
         MatToolbarModule,
     ],
 })
