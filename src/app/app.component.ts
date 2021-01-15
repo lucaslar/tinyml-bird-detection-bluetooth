066216898d31e7a5f82727d5378822eb5292736e
Lucas Larisch <lucas.larisch@googlemail.com>
2021-01-15 02:47:43 +0100

    Onboarding screen only shown when loading the page on a device for the first time.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
-import { BluetoothService } from './services/bluetooth.service';
+import { Component, OnInit } from '@angular/core';
 import { I18nService } from './services/i18n.service';
 import { ThemingService } from './services/theming.service';
 import { MatDialog } from '@angular/material/dialog';
 import { InfoSlidesComponent } from './components/dialogs/info-slides/info-slides.component';
+import { StorageService } from './services/storage.service';
 
 /**
  * Central component of the application (SPA).
@@ -13,14 +13,21 @@ import { InfoSlidesComponent } from './components/dialogs/info-slides/info-slide
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     constructor(
-        readonly bluetooth: BluetoothService,
         readonly theming: ThemingService,
-        i18n: I18nService,
-        dialog: MatDialog
-    ) {
-        i18n.initialize();
-        dialog.open(InfoSlidesComponent, { disableClose: true });
+        private readonly i18n: I18nService,
+        private readonly dialog: MatDialog,
+        private readonly storage: StorageService
+    ) {}
+
+    ngOnInit(): void {
+        this.i18n.initialize();
+        if (!this.storage.isOnboarded) {
+            this.dialog
+                .open(InfoSlidesComponent, { disableClose: true })
+                .afterClosed()
+                .subscribe(() => (this.storage.isOnboarded = true));
+        }
     }
 }
--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -20,6 +20,12 @@ export class StorageService {
     private keyIsDarkTheme = 'is_dark_theme_set';
 
     /**
+     * Key for a boolean value whether the user (device) is onboarded or not.
+     * @private
+     */
+    private keyIsOnboarded = 'is_onboarded';
+
+    /**
      * @returns Stored language ID.
      */
     get language(): string {
@@ -35,19 +41,36 @@ export class StorageService {
     }
 
     /**
-     * @returns true if the dark mode is stored in the database, false if not.
+     * @returns true if the dark mode is stored in the local storage, false if not.
      */
     get isDarkTheme(): boolean {
         return !!localStorage.getItem(this.keyIsDarkTheme);
     }
 
     /**
-     * @param value If true, a value will be stored in the local storage, otherwise
-     *        it will be deleted.
+     * @param value If true, a value indicating dark mode is to be displayed will be
+     *        stored in the local storage, otherwise it will be deleted.
      */
     set isDarkTheme(value: boolean) {
         value
             ? localStorage.setItem(this.keyIsDarkTheme, '1')
             : localStorage.removeItem(this.keyIsDarkTheme);
+    }
+
+    /**
+     * @returns true if the user (device) has already been onboarded, false if not.
+     */
+    get isOnboarded(): boolean {
+        return !!localStorage.getItem(this.keyIsOnboarded);
+    }
+
+    /**
+     * @param value If true, a value indicating the user (device) has been onboarded will
+     *        be stored in the local storage, otherwise it will be deleted.
+     */
+    set isOnboarded(value: boolean) {
+        value
+            ? localStorage.setItem(this.keyIsOnboarded, '1')
+            : localStorage.removeItem(this.keyIsOnboarded);
     }
 }
