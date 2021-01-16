9210651f36f1a2ad4ae9ec08e6892a5e182d61c9
Lucas Larisch <lucas.larisch@googlemail.com>
2021-01-16 01:25:47 +0100

    Added language selection to onboarding dialog.

--- src/app/components/dialogs/info-slides/info-slides.component.ts
+++ src/app/components/dialogs/info-slides/info-slides.component.ts
@@ -7,6 +7,9 @@ import {
     ViewChild,
 } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { heightTransition } from '../../../consts/animations';
+import { Language } from '../../../model/internal/language';
+import { I18nService } from '../../../services/i18n.service';
 
 type slide = {
     title: string;
@@ -19,6 +22,7 @@ type slide = {
  */
 @Component({
     selector: 'app-info-slides',
+    animations: [heightTransition],
     templateUrl: './info-slides.component.html',
     styleUrls: ['./info-slides.component.scss'],
 })
@@ -32,6 +36,11 @@ export class InfoSlidesComponent implements OnInit {
      * Highest selected index.
      */
     highestClickedIndex = 0;
+
+    /**
+     * True if the language selection dialog is to be displayed, false if not.
+     */
+    isLanguagesDisplayed = false;
 
     // TODO Remove?
     readonly slides: slide[];
@@ -51,8 +60,12 @@ export class InfoSlidesComponent implements OnInit {
 
     /**
      * @param data Injected data
+     * @param i18n Injected internationalization service.
      */
-    constructor(@Inject(MAT_DIALOG_DATA) data: { isOnboarding: boolean }) {
+    constructor(
+        @Inject(MAT_DIALOG_DATA) data: { isOnboarding: boolean },
+        private readonly i18n: I18nService
+    ) {
         this.isOnboarding = data?.isOnboarding;
         this.slides = [];
         [
@@ -97,9 +110,22 @@ export class InfoSlidesComponent implements OnInit {
      * @param index Index to be set as active (if legal).
      */
     onPaginationClicked(index: number): void {
-        if (!this.isOnboarding || index <= this.highestClickedIndex) {
+        if (
+            !this.isLanguagesDisplayed &&
+            (!this.isOnboarding || index <= this.highestClickedIndex)
+        ) {
             this.activeIndex = index;
         }
+    }
+
+    /**
+     * Changes the language and hides the language selection
+     *
+     * @param language Language selected by the user.
+     */
+    onLanguageSelected(language: Language): void {
+        this.isLanguagesDisplayed = false;
+        this.i18n.useLanguage(language);
     }
 
     /**
@@ -108,10 +134,15 @@ export class InfoSlidesComponent implements OnInit {
      * @param event Keydown event.
      */
     @HostListener('document:keydown', ['$event'])
-    onKeydown(event: KeyboardEvent): void {
-        if (event.key === 'ArrowLeft' && this.activeIndex - 1 >= 0) {
+    private onKeydown(event: KeyboardEvent): void {
+        if (
+            !this.isLanguagesDisplayed &&
+            event.key === 'ArrowLeft' &&
+            this.activeIndex - 1 >= 0
+        ) {
             this.activeIndex--;
         } else if (
+            !this.isLanguagesDisplayed &&
             event.key === 'ArrowRight' &&
             this.activeIndex + 1 < this.slides.length
         ) {
