83c19e1dc8f4b7c2c1f763111b3c034e473167b1
Lucas Larisch <lucas.larisch@googlemail.com>
2021-01-17 17:16:50 +0100

    Adjusted "under construction" placement and updated further information.

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { BluetoothService } from '../../services/bluetooth.service';
 
+/**
+ * Component containing main content to be shown and logic which content is to be shown in which case.
+ */
 @Component({
     selector: 'app-main-content',
     templateUrl: './main-content.component.html',
