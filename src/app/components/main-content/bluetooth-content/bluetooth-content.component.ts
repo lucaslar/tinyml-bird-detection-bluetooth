a49dad9d2b5a6b350c303dca97d9035008a11b7f
Lucas Larisch <lucas.larisch@googlemail.com>
2021-01-18 02:34:41 +0100

    Implemented basic content view.

--- src/app/components/main-content/bluetooth-content/bluetooth-content.component.ts
+++ src/app/components/main-content/bluetooth-content/bluetooth-content.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BluetoothService } from '../../../services/bluetooth.service';
+import { faKiwiBird } from '@fortawesome/free-solid-svg-icons';
 
 /**
  * Component for displaying the results emitted by the Bluetooth device.
@@ -10,6 +11,10 @@ import { BluetoothService } from '../../../services/bluetooth.service';
     styleUrls: ['./bluetooth-content.component.scss'],
 })
 export class BluetoothContentComponent {
+    /**
+     * Icon to be displayed next to the values.
+     */
+    readonly birdIcon = faKiwiBird;
     /**
      * @param bluetooth Injected Bluetooth service.
      */
